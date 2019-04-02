596acd2b0ffac56c5ce28d68c8f443203bb5d339
Mike Peachey <mike.peachey@bjss.com>
2019-04-02 14:53:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,202 @@
+# bashlog
+
+A simple but powerful logging library for bash
+
+## Simple Usage
+
+Place log.sh in the directory with your bash script(s)
+
+Add `. log.sh` to the top of your script(s)
+
+Add logging lines with level info, warn, error or debug, for example:
+
+  * `log info 'My info message';`
+
+  * `log warn 'My warning message';`
+
+  * `log error 'My error message';`
+
+  * `log debug 'My debugging message';`
+
+
+## Bash Shell Options
+
+This library sets the following shell options:
+
+`set -uo pipefail`
+
+This is a recommended default for any scripts you write:
+  * You should not use unbound variables (-u)
+  * You should ensure pipelines fail if any of the commands in the pipeline fail (-o pipefail)
+
+This library does _not_ `set -e` because automatically exiting a script
+when any error is encountered is lazy and a poor user experience.
+Errors should be caught and handled by your scripts.
+This can be supported by the use of this library's 'error' log-level.
+
+If you do not like these settings - change them or remove them.
+
+## Debugging
+
+To enable debugging, set `DEBUG=1` in your script's environment.
+
+  * When `DEBUG=0`, log lines with level 'debug' are not printed.
+  * When `DEBUG=1`, log lines with level 'debug' are printed.
+
+### Interactive debugging of errors
+
+When debugging is enabled, error messages are followed by an interactive
+bash shell from which debugging can take place.
+
+When debugging activity is complete: exiting the shell with code 0
+will allow the rest of your script to continue, exiting the shell
+with a non-zero code will cause your script to terminate with the
+same non-zero code.
+
+### Bash Debug Trap
+
+This library enables a bash DEBUG trap, that provides you with
+a variable called `prev_cmd` to help you write simple and useful
+debug logging messages.
+
+For example:
+
+```
+my_command --with very -c -o -m -p -l -e -x -- parameters \
+  && log debug "${prev_cmd}" \
+  || log error "${prev_cmd};
+```
+
+This will log the exact command that was executed as a debug log
+(only when `DEBUG=1`).
+
+If the command fails, the error message will show the precise command
+that failed, and when `DEBUG=1` will then present you with a debug
+shell from which you can choose to continue or to exit your script.
+
+#### Advanced Debugging
+
+This library contains an alternate debug trap that can be uncommented
+which will log a debug log entry (when `DEBUG=1`) for every single
+command that is executed throughout your script.
+
+Enabling this alternate trap will severely impact performance of
+your script, and the bashlog testing suite will not be applicable.
+
+## File, JSON & Syslog
+
+When instructed, the logging library can also log output:
+  * To a file in plain text format: `BASHLOG_FILE=1`
+  * To a file in JSON format: `BASHLOG_JSON=1`
+  * To Syslog: `BASHLOG_SYSLOG=1`
+
+## Additional environment variables
+
+### BASHLOG_DATE_FORMAT
+
+Default: `+%F %T`
+
+The date format to use for stdout and file logging (passed to the `date` binary).
+
+Syslog date format is determined by syslog configuration.
+
+JSON date format is epoch.
+
+### BASHLOG_FILE_PATH
+
+Default: `/tmp/$(basename {0}).log`
+
+When `BASHLOG_FILE=1`, logs are written to the file at `$BASHLOG_FILE_PATH`
+
+This defaults to the name of your script (the one from which you sourced this library),
+with a `.log` suffix, in the /tmp directory.
+
+e.g. yourscript.sh will produce `/tmp/yourscript.sh.log`
+
+### BASHLOG_JSON_PATH
+
+Default: `/tmp/$(basename {0}).log.json`
+
+When `BASHLOG_JSON=1`, logs are written in JSON format to the file at `$BASHLOG_JSON_PATH`
+
+This defaults to the name of your script (the one from which you sourced this library),
+with a `.log.json` suffix, in the /tmp directory.
+
+e.g. yourscript.sh will produce `/tmp/yourscript.sh.log.json`
+
+### BASHLOG_SYSLOG_FACILITY
+
+Default: `local0`
+
+When `BASHLOG_SYSLOG=1`, logs are written to Syslog.
+
+This determines the syslog facility to use (local0 <-> local7)
+
+### BASHLOG_SYSLOG_TAG
+
+Default: `$(basename {0})`
+
+When BASHLOG_SYSLOG=1, logs are written to Syslog.
+
+This determines the syslog tag to use, defaulting to the name of your script,
+e.g. `yourscript.sh`
+
+## Recommended Usage
+
+```
+. log.sh;
+
+cat << 'EOF' > /etc/yum.repos.d/smoo.repo &&
+[smoo]
+name=smoo
+baseurl=file:///tmp/smoo/
+enabled=1
+gpgcheck=0
+EOF
+  log debug "Smoo repo yum repository configuration written successfully" \
+  || log error "Failed to write smoo repo yum repository configuration";
+
+
+# Install the smoo agent
+yum -y install smoo-agent \
+  && log info "smoo-agent RPM installed successfully" \
+  || log error "Failed to install smoo-agent RPM";
+
+# Change into the newly installed repository
+cd /srv/smoo \
+  && log debug "${prev_cmd}" \
+  || log error "${prev_cmd}";
+
+/opt/smoo-agent/bin/tests /srv/smoo \
+  && log info "Smoo Tests Succeeded" \
+  || log error "Smoo Tests Failed";
+
+# Execute the smoo-agent
+/opt/smoo-agent/bin/smoo \
+  -r \
+  --config config.json \
+  --log_level "info" \
+  && log info "Smoo Agent execution completed successfully" \
+  || log error "Smoo Agent execution failed!";
+
+if [ "${DEBUG:-0}" -eq "1" ]; then
+  log debug "Entering end-of-script debug shell. Exit shell to continue.";
+  /bin/bash;
+fi;
+
+log info 'Smoo Agent control finished';
+
+exit 0;
+```
+
+## Testing
+
+A full test suite is provided alongside the bashlog library in `test.sh`.
+
+The test suite itself has been tested on Slackware Linux. If your syslog
+configuration behaves differently to the default in Slackware, the test
+suite may fail. Feel free to provide feedback / pull requests to improve it.
+
+The test suite requires sudo privileges in order to create files that
+cannot then be accessed. I could replace this with non-root privilege
+modification - and maybe I will - but not today.
---
+++ test.sh
@@ -0,0 +1,468 @@
+#!/bin/bash
+
+set -uo pipefail;
+
+# Interactive debug testing is off by default.
+# To turn it on, pass TEST_INTERACTIVE=1
+# Correct exit code from this test script
+# will then be '1' instead of '0'
+declare interactive="${TEST_INTERACTIVE:-0}";
+
+function result() {
+  local level="${1}";
+  shift;
+  local line="${@}"; 
+
+  case "${level}" in
+    ok)
+      echo -e "\t\033[32mOK: ${line}\033[0m";
+      ;;
+    fail)
+      echo -e "\t\033[31mFAIL: ${line}\033[0m";
+      echo -e "Abandoning tests due to failure";
+      exit 1;
+      ;;
+    *)
+      echo "UWOTM8?!";
+      exit 1;
+      ;;
+  esac;
+}
+
+sudo rm -f /tmp/${0}.log;
+sudo rm -f /tmp/${0}.log.json;
+
+source log.sh;
+
+declare random_string="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)";
+declare stdout;
+declare fileout;
+declare jsonout;
+declare syslogout;
+
+declare BASHLOG_FILE=1;
+declare BASHLOG_JSON=1;
+declare BASHLOG_SYSLOG=1;
+
+##
+# INFO
+##
+
+echo "Testing 'info'";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=0;
+
+stdout="$(log 'info' "${random_string}")";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/messages)";
+
+grep -q -E $'^\033\[32m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[INFO\] '"${random_string}"$'\033\[0m$' <<<"${stdout}" \
+  && result ok 'info -> stdout' \
+  || result fail 'info -> stdout';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[INFO\] ${random_string}$" <<<"${fileout}" \
+  && result ok 'info -> file' \
+  || result fail 'info -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"info","message":"'"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'info -> json file' \
+  || result fail 'info -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: INFO: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'info -> syslog (/var/log/messages)' \
+  || result fail 'info -> syslog (/var/log/messages)';
+
+##
+# WARN
+##
+
+echo "Testing 'warn'";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=0;
+
+stdout="$(log 'warn' "${random_string}")";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/syslog)";
+
+grep -q -E $'^\033\[33m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[WARN\] '"${random_string}"$'\033\[0m$' <<<"${stdout}" \
+  && result ok 'warn -> stdout' \
+  || result fail 'warn -> stdout';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[WARN\] ${random_string}$" <<<"${fileout}" \
+  && result ok 'warn -> file' \
+  || result fail 'warn -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"warn","message":"'"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'warn -> json file' \
+  || result fail 'warn -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: WARN: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'warn -> syslog (/var/log/syslog)' \
+  || result fail 'warn -> syslog (/var/log/syslog)';
+
+##
+# ERROR
+##
+
+echo "Testing: 'error'";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=0;
+
+stderr="$(log 'error' "${random_string}" 2>&1 1>/dev/null)";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/syslog)";
+
+grep -q -E $'^\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] '"${random_string}"$'\033\[0m$' <<<"${stderr}" \
+  && result ok 'error -> stderr' \
+  || result fail 'error -> stderr';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] ${random_string}$" <<<"${fileout}" \
+  && result ok 'error -> file' \
+  || result fail 'error -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"error","message":"'"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'error -> json file' \
+  || result fail 'error -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: ERROR: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'error -> syslog (/var/log/syslog)' \
+  || result fail 'error -> syslog (/var/log/syslog)';
+
+##
+# DEBUG OFF
+##
+
+echo "Testing 'debug', DEBUG=0";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=0;
+
+# If there's no output, there'll be no file
+touch /tmp/${0}.log;
+touch /tmp/${0}.log.json;
+
+stdout="$(log 'debug' "${random_string}")";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/debug)";
+
+grep -q -E $'^\033\[34m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[DEBUG\] '"${random_string}"$'\033\[0m$' <<<"${stdout}" \
+  && result fail 'debug -> stdout' \
+  || result ok 'debug -> stdout';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[DEBUG\] ${random_string}$" <<<"${fileout}" \
+  && result fail 'debug -> file' \
+  || result ok 'debug -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"debug","message":"'"${random_string}"'"}$' <<<"${jsonout}" \
+  && result fail 'debug -> json file' \
+  || result ok 'debug -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: DEBUG: ${random_string}$" <<<"${syslogout}" \
+  && result fail 'debug -> syslog (/var/log/debug)' \
+  || result ok 'debug -> syslog (/var/log/debug)';
+
+##
+# DEBUG ON
+##
+
+echo "Testing 'debug', DEBUG=1";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=1;
+
+stdout="$(log 'debug' "${random_string}")";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/debug)";
+
+grep -q -E $'^\033\[34m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[DEBUG\] '"${random_string}"$'\033\[0m$' <<<"${stdout}" \
+  && result ok 'debug -> stdout' \
+  || result fail 'debug -> stdout';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[DEBUG\] ${random_string}$" <<<"${fileout}" \
+  && result ok 'debug -> file' \
+  || result fail 'debug -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"debug","message":"'"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'debug -> json file' \
+  || result fail 'debug -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: DEBUG: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'debug -> syslog (/var/log/debug)' \
+  || result fail 'debug -> syslog (/var/log/debug)';
+
+##
+# BAD LEVEL, DEBUG OFF
+##
+
+echo "Testing: BAD LEVEL ('snooch'), DEBUG=0";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=0;
+
+stderr="$(log 'snooch' "${random_string}" 2>&1 1>/dev/null)";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/syslog)";
+
+grep -q -E $'^\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Undefined log level trying to log: '"${random_string}"$'\033\[0m$' <<<"${stderr}" \
+  && result ok 'snooch -> stderr' \
+  || result fail 'snooch -> stderr';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Undefined log level trying to log: ${random_string}$" <<<"${fileout}" \
+  && result ok 'snooch -> file' \
+  || result fail 'snooch -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"error","message":"Undefined log level trying to log: '"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'snooch -> json file' \
+  || result fail 'snooch -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: ERROR: Undefined log level trying to log: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'snooch -> syslog (/var/log/syslog)' \
+  || result fail 'snooch -> syslog (/var/log/syslog)';
+
+##
+# BAD LEVEL, DEBUG ON
+##
+
+echo "Testing: BAD LEVEL ('snooch'), DEBUG=1";
+
+rm -f /tmp/${0}.log;
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=1;
+DEBUG=1;
+
+stderr="$(echo | log 'snooch' "${random_string}" 2>&1 1>/dev/null)";
+fileout="$(tail -n1 /tmp/${0}.log)";
+jsonout="$(tail -n1 /tmp/${0}.log.json)";
+syslogout="$(sudo tail -n1 /var/log/syslog)";
+
+grep -q -E $'^\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Undefined log level trying to log: '"${random_string}"$'\033\[0m$' <<<"${stderr}" \
+  && result ok 'snooch -> stderr' \
+  || result fail 'snooch -> stderr';
+
+grep -q -E "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Undefined log level trying to log: ${random_string}$" <<<"${fileout}" \
+  && result ok 'snooch -> file' \
+  || result fail 'snooch -> file';
+
+grep -q -E '^{"timestamp":"[0-9]{10}","level":"error","message":"Undefined log level trying to log: '"${random_string}"'"}$' <<<"${jsonout}" \
+  && result ok 'snooch -> json file' \
+  || result fail 'snooch -> json file';
+
+grep -q -E "$(basename ${0})\[${$}\]: ERROR: Undefined log level trying to log: ${random_string}$" <<<"${syslogout}" \
+  && result ok 'snooch -> syslog (/var/log/syslog)' \
+  || result fail 'snooch -> syslog (/var/log/syslog)';
+
+##
+# INFO, FILE IO EXCEPTION, DEBUG OFF
+##
+
+echo "Testing: 'info', IO Exception (file), DEBUG=0";
+
+rm -f /tmp/${0}.log;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=0;
+BASHLOG_SYSLOG=0;
+DEBUG=0;
+
+sudo touch /tmp/${0}.log;
+
+stderr="$(log 'info' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[INFO\] '"${random_string}"'" >> "/tmp/'"$(basename ${0})"$'.log"\033\[0m$' <<<"${stderr}" \
+  && result ok 'info -> file, Permission denied -> stderr' \
+  || result fail 'info -> file, Permission denied -> stderr';
+
+##
+# INFO, FILE IO EXCEPTION, DEBUG ON
+##
+
+echo "Testing: 'info', IO Exception (file), DEBUG=1";
+
+rm -f /tmp/${0}.log;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=0;
+BASHLOG_SYSLOG=0;
+DEBUG=1;
+
+sudo touch /tmp/${0}.log;
+
+stderr="$(echo | log 'info' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[INFO\] '"${random_string}"'" >> "/tmp/'"$(basename ${0})"$'.log"\033\[0m$' <<<"${stderr}" \
+  && result ok 'info -> file, Permission denied -> stderr' \
+  || result fail 'info -> file, Permission denied -> stderr';
+
+##
+# INFO, JSON FILE IO EXCEPTION, DEBUG OFF
+##
+
+echo "Testing: 'info', IO Exception (json), DEBUG=0";
+
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=0;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=0;
+DEBUG=0;
+
+sudo touch /tmp/${0}.log.json;
+
+stderr="$(log 'info' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log.json;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log.json: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "{"timestamp":"[0-9]{10}","level":"info","message":"'"${random_string}"'"}" >> "/tmp/'"$(basename ${0})"$'.log.json"\033\[0m$' <<<"${stderr}" \
+  && result ok 'info -> file, Permission denied -> stderr' \
+  || result fail 'info -> file, Permission denied -> stderr';
+
+##
+# INFO, JSON FILE IO EXCEPTION, DEBUG ON
+##
+
+echo "Testing: 'info', IO Exception (json), DEBUG=1";
+
+rm -f /tmp/${0}.log.json;
+
+BASHLOG_FILE=0;
+BASHLOG_JSON=1;
+BASHLOG_SYSLOG=0;
+DEBUG=1;
+
+sudo touch /tmp/${0}.log.json;
+
+stderr="$(echo | log 'info' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log.json;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log.json: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "{"timestamp":"[0-9]{10}","level":"info","message":"'"${random_string}"'"}" >> "/tmp/'"$(basename ${0})"$'.log.json"\033\[0m$' <<<"${stderr}" \
+  && result ok 'info -> file, Permission denied -> stderr' \
+  || result fail 'info -> file, Permission denied -> stderr';
+
+##
+# WARN, FILE IO EXCEPTION, DEBUG OFF
+##
+
+echo "Testing: 'warn', IO Exception (file), DEBUG=0";
+
+rm -f /tmp/${0}.log;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=0;
+BASHLOG_SYSLOG=0;
+DEBUG=0;
+
+sudo touch /tmp/${0}.log;
+
+stderr="$(log 'warn' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[WARN\] '"${random_string}"'" >> "/tmp/'"$(basename ${0})"$'.log"\033\[0m$' <<<"${stderr}" \
+  && result ok 'warn -> file, Permission denied -> stderr' \
+  || result fail 'warn -> file, Permission denied -> stderr';
+
+##
+# ERROR, FILE IO EXCEPTION, DEBUG OFF
+##
+
+echo "Testing: 'error', IO Exception (file), DEBUG=0";
+
+rm -f /tmp/${0}.log;
+
+BASHLOG_FILE=1;
+BASHLOG_JSON=0;
+BASHLOG_SYSLOG=0;
+DEBUG=0;
+
+sudo touch /tmp/${0}.log;
+
+stderr="$(log 'error' "${random_string}" 2>&1 1>/dev/null)";
+
+sudo rm -f /tmp/${0}.log;
+
+grep -q -E $'^./log.sh: line [0-9]+: /tmp/'"$(basename ${0})"'.log: Permission denied' <<<"${stderr}" \
+  && grep -q -E $'\033\[31m[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] Logging Exception: echo -e "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\] '"${random_string}"'" >> "/tmp/'"$(basename ${0})"$'.log"\033\[0m$' <<<"${stderr}" \
+  && result ok 'error -> file, Permission denied -> stderr' \
+  || result fail 'error -> file, Permission denied -> stderr';
+
+##
+# INTERACTIVE DEBUG
+##
+
+if [ "${interactive}" -eq 1 ]; then
+
+  echo "Testing: Debug Interaction, DEBUG=1";
+
+  BASHLOG_FILE=0;
+  BASHLOG_JSON=0;
+  BASHLOG_SYSLOG=0;
+  DEBUG=1;
+
+  echo -e "\n\t\033[32mLogging a normal successful debug message to stdout\033[0m";
+  log 'debug' 'A normal successful debug message';
+
+  echo -e "\n\t\033[32mLogging an error message to stdout that should provide a debug shell\n\tExit the shell with ^D, 'exit 0' or 'exit'.\n\tIf you exit with a non-zero code, testing will be abandoned, this script will exit without further warning.\033[0m";
+  log 'error' 'An error message';
+
+  result ok 'Interactive Shell. We have errored and continued.';
+
+  echo -e "\n\t\033[32mLogging an error message to stdout that should provide a debug shell\n\tExit the shell with 'exit 1' or a non-zero code of your choice.\n\tThe test is successful if this script exits with the same code\033[0m";
+  log 'error' 'An error message';
+
+  result fail 'Interactive Shell. This script should have exited with a non-zero code';
+
+fi;
+
+exit 0;
